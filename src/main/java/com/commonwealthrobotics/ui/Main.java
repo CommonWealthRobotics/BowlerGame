44c7662c10975885f91ffb470e259d73a8788f4d
Kevin Harrington <mad.hephaestus@gmail.com>
2022-09-07 18:37:50 -0400

    adding fxml loading

--- src/main/java/com/commonwealthrobotics/ui/Main.java
+++ src/main/java/com/commonwealthrobotics/ui/Main.java
@@ -1,6 +1,6 @@
 package com.commonwealthrobotics.ui;
 
-public class Main {
+public class Main extends Application  {
 
 	public static void main(String[] args) {
 		System.out.println("BowlerGame Main");
