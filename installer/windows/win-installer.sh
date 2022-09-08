e11b26be140a70ff5be54076c65df111114c584a
Kevin Harrington <mad.hephaestus@gmail.com>
2022-09-07 22:15:27 -0400

    add print arroung the very silent install

--- installer/windows/win-installer.sh
+++ installer/windows/win-installer.sh
@@ -1,3 +1,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+echo "Installing Inno IDE"
 $SCRIPT_DIR/isetup-5.4.3.exe /verysilent /allusers
+echo "Done Installing Inno IDE"
